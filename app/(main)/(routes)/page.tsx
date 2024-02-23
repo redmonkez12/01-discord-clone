bbfe566939e0ad1bbc45226d8ee87c9c2442cf30
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2024-02-23 07:38:48 +0100

    Init db

--- app/(main)/(routes)/page.tsx
+++
@@ -1,9 +0,0 @@
-import { UserButton } from "@clerk/nextjs";
-
-export default function Page() {
-    return (
-        <div>
-            <UserButton afterSignOutUrl="/"/>
-        </div>
-    );
-}
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,9 @@ import { Open_Sans } from "next/font/google";
 
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
-const inter = Open_Sans({ subsets: ["latin"] });
+const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Team Chat Application",
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={cn(
+          font.className,
+          "bg-white dark:bg-[#313338]"
+      )}>
         <ClerkProvider>
           <ThemeProvider
             attribute="class"
