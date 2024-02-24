988bf0a67753de22dc018e56fdaa2260b7c94e70
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2024-02-24 16:51:42 +0100

    Create server

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -17,7 +17,7 @@ export default async function SetupPage() {
     });
 
     if (server) {
-        return redirect(`/servers/${server.id} `);
+        return redirect(`/servers/${server.id}`);
     }
 
     return (
--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -17,6 +17,8 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
 import { FileUpload } from "@/components/file-upload";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -29,6 +31,8 @@ const formSchema = z.object({
 
 export function InitialModal() {
     const [isMounted, setIsMounted] = useState(false);
+
+    const router = useRouter();
 
     useEffect(() => {
         setIsMounted(true)
@@ -45,7 +49,15 @@ export function InitialModal() {
     const isLoading = form.formState.isSubmitting;
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values);
+        try {
+            await axios.post("/api/servers", values)
+
+            form.reset()
+            router.refresh();
+            window.location.reload();
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     if (!isMounted) {
