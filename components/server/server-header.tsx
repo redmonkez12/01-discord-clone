ee157ccba49a1893114ffe11bd8a3d79b01b4902
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2024-02-25 08:24:17 +0100

    Add invite modal

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -53,6 +53,7 @@ export const ServerHeader = ({
             >
                 {isModerator && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("invite", { server })}
                         className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
                     >
                         Invite People
