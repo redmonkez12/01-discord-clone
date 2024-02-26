8a1d1c298ed7fe5cb182e195e58ccddec63d0cb5
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2024-02-26 09:02:26 +0100

    Add create and edit modal

--- components/modals/delete-channel-modal.tsx
+++ components/modals/delete-channel-modal.tsx
@@ -11,13 +11,14 @@ import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import qs from "query-string";
 
-export const DeleteServerModal = () => {
+export const DeleteChannelModal  = () => {
     const { isOpen, onClose, type, data } = useModal();
     const router = useRouter();
 
-    const isModalOpen = isOpen && type === "deleteServer";
-    const { server } = data;
+    const isModalOpen = isOpen && type === "deleteChannel";
+    const { server, channel } = data;
 
     const [isLoading, setIsLoading] = useState(false);
 
@@ -25,11 +26,18 @@ export const DeleteServerModal = () => {
         try {
             setIsLoading(true);
 
-            await axios.delete(`/api/servers/${server?.id}`);
+            const url = qs.stringifyUrl({
+                url: `/api/channels/${channel?.id}`,
+                query: {
+                    serverId: server?.id,
+                }
+            })
+
+            await axios.delete(url);
 
             onClose();
-            router.refresh()
-            router.push("/")
+            router.push(`/servers/${server?.id}`);
+            router.refresh();
         } catch (error) {
             console.log(error);
         } finally {
@@ -42,11 +50,11 @@ export const DeleteServerModal = () => {
             <DialogContent className="bg-white text-black p-0 overflow-hidden">
                 <DialogHeader className="pt-8 px-6">
                     <DialogTitle className="text-2xl text-center font-bold">
-                        Delete Server
+                        Delete Channel
                     </DialogTitle>
                     <DialogDescription className="text-center text-zin-500">
                         Are you sure you want to do this? <br/>
-                        <span className="text-indigo-500 font-semibold">{server?.name}</span> will be permanently deleted.
+                        <span className="text-indigo-500 font-semibold">#{channel?.name}</span> will be permanently deleted.
                     </DialogDescription>
                 </DialogHeader>
 
