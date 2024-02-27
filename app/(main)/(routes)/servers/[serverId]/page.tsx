093ae856af73944deef24fa68d9796079be70743
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2024-02-27 13:14:26 +0100

    Default channel

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,5 +1,47 @@
-export default function Page() {
-    return (
-        <div></div>
-    );
+import { redirectToSignIn } from "@clerk/nextjs";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+type ServerIdPageProps = {
+    params: {
+        serverId: string;
+    }
+};
+
+export default async function Page({ params }: ServerIdPageProps) {
+    const profile = await currentProfile();
+
+    if (!profile) {
+        return redirectToSignIn();
+    }
+
+    const server = await db.server.findUnique({
+        where: {
+            id: params.serverId,
+            members: {
+                some: {
+                  profileId: profile.id,
+                },
+            },
+        },
+        include: {
+            channels: {
+                where: {
+                    name: "general",
+                },
+                orderBy: {
+                    createdAt: "asc",
+                },
+            },
+        },
+    });
+
+    const initialChannel = server?.channels[0];
+
+    if (initialChannel?.name !== "general") {
+        return null;
+    }
+
+    return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`);
 }
