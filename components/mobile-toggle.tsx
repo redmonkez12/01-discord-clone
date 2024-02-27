20ce1ec281469e68b394877a0fe2d0dd2d403c76
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2024-02-27 13:38:03 +0100

    Chat header

---
+++ components/mobile-toggle.tsx
@@ -0,0 +1,27 @@
+import { Menu } from "lucide-react";
+import { SheetContent, SheetTrigger, Sheet } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
+import { ServerSidebar } from "@/components/server/server-sidebar";
+
+type MobileToggleProps = {
+    serverId: string;
+};
+
+export function MobileToggle({ serverId }: MobileToggleProps) {
+    return (
+        <Sheet>
+            <SheetTrigger asChild>
+                <Button variant="ghost" size="icon" className="md:hidden">
+                    <Menu/>
+                </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="p-0 flex gap-0">
+                <div className="w-[72px]">
+                    <NavigationSidebar/>
+                </div>
+                <ServerSidebar serverId={serverId}/>
+            </SheetContent>
+        </Sheet>
+    );
+}
